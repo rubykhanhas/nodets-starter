3dcc500ad64e562c9da7541c5cd0e8bb5933a8af
linux <rubykhanhas@gmail.com>
2020-11-23 19:37:36 +0700

    cofig watch mode

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-/* Root file */
+//#region Tips
+/* 
+    import absolute path with {
+        '@' : './src',
+        '~' : './',
+    }
+    eg: import {component} from '@/components/component.ts',
+        import express from '~/node_modules/express/index.ts,
+*/
+//#endregion
+
+console.log("Hello world!")
